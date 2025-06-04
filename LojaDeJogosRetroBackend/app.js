3ea52f6b5cb4112901819371efc647053538a6f3
João Pablo <jp684160@gmail.com>
2025-06-04 13:19:11 -0300

    usando o bd MongoDB Atlas

--- LojaDeJogosRetroBackend/app.js
+++ LojaDeJogosRetroBackend/app.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/loja");
 require("dotenv").config();
-
-const app = express();
 const cors = require("cors");
+
+// Conexão com MongoDB Atlas
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("✅ Conectado ao MongoDB Atlas com sucesso!"))
+  .catch((err) => console.error("❌ Erro ao conectar no MongoDB Atlas:", err));
+
+// Inicializar o app
+const app = express();
 
 // Importar rotas e middleware
 const GameRouter = require("./src/Routers/GameRouter");
 const userRouter = require("./src/Routers/UserRouter");
 const MiddlewareAuth = require("./MiddlewareAuth");
 
+// Middlewares globais
 app.use(express.json());
 app.use(cors());
 
 // Rotas públicas
 app.use("/api/users", userRouter);
 
-// middleware
+// Middleware de autenticação
 app.use(MiddlewareAuth);
+
+// Rotas privadas
 app.use(GameRouter);
 
-const PORT = 3000;
-app.listen(80, () => {
-  console.log(`Sua aplicação está sendo executada na porta ${80}`);
+// Definir porta
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
