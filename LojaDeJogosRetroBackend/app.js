ea1526482af098ba4a03f93639dbe580fba9ae4c
João Pablo <jp684160@gmail.com>
2025-06-04 12:12:01 -0300

    alteração na porta

--- LojaDeJogosRetroBackend/app.js
+++ LojaDeJogosRetroBackend/app.js
@@ -4,24 +4,24 @@ mongoose.connect("mongodb://localhost:27017/loja");
 require("dotenv").config();
 
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 // Importar rotas e middleware
-const GameRouter = require('./src/Routers/GameRouter');
-const userRouter = require('./src/Routers/UserRouter');
+const GameRouter = require("./src/Routers/GameRouter");
+const userRouter = require("./src/Routers/UserRouter");
 const MiddlewareAuth = require("./MiddlewareAuth");
 
 app.use(express.json());
 app.use(cors());
 
-// Rotas públicas (sem autenticação)
-app.use('/api/users', userRouter); // Ex: Login, Cadastro
+// Rotas públicas
+app.use("/api/users", userRouter);
 
-// Aplicar middleware APENAS para rotas protegidas
+// middleware
 app.use(MiddlewareAuth);
-app.use(GameRouter); // Ex: Jogos
+app.use(GameRouter);
 
-const PORT = 3000; // Altere para 3000
-app.listen(PORT, () => {
-  console.log(`Sua aplicação está sendo executada na porta ${PORT}`);
+const PORT = 3000;
+app.listen(80, () => {
+  console.log(`Sua aplicação está sendo executada na porta ${80}`);
 });
